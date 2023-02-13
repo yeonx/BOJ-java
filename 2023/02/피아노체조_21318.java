9ea19d26966f2a8e2689631b5ad53b11ef72f38f
yeonx <71878202+yeonx@users.noreply.github.com>
2023-02-13 17:12:49 +0900

    Create 피아노체조_21318.java

---
+++ 2023/02/피아노체조_21318.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_21318 {
+
+	static int N,Q;
+	static int[] music;
+	static int[] count;
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		music = new int[N+1];
+		count = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=N;i++) {
+			music[i] = Integer.parseInt(st.nextToken());
+			if(music[i] < music[i-1]) {
+				count[i] = count[i-1]+1;
+			}else {
+				count[i] = count[i-1];
+			}
+		}
+
+		Q = Integer.parseInt(br.readLine());
+	
+		for(int i=0;i<Q;i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			System.out.println(count[y]-count[x]);
+		}
+	}
+}

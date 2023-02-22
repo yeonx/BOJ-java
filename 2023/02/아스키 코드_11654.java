20c18e9020991bdb3cf379a2d07ad372a7f253a9
yeonx <71878202+yeonx@users.noreply.github.com>
2023-02-22 12:12:06 +0900

    Create 아스키 코드_11654.java

---
+++ 2023/02/아스키 코드_11654.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_11654 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+		String s = br.readLine();
+		
+		System.out.println((int)s.charAt(0));
+	}
+}

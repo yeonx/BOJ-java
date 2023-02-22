0f985717c8d2e8ca4ee83e24794569c5accebaa0
yeonx <71878202+yeonx@users.noreply.github.com>
2023-02-22 17:32:49 +0900

    Create 최대힙_11279.java

---
+++ 2023/02/최대힙_11279.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_11279 {
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<N;i++) {
+			int number = Integer.parseInt(br.readLine());
+			
+			if(number==0) {
+				if(pq.isEmpty()) {
+					sb.append(0 + "\n");
+				}else {
+					sb.append(pq.poll() + "\n");
+				}
+			} else {
+				pq.offer(number);
+			}
+		}
+		System.out.println(sb);
+	
+	}
+
+}

ea443098133af4b5e9551c5fab3e10b310acda3a
yeonx <71878202+yeonx@users.noreply.github.com>
2023-03-01 12:17:51 +0900

    Create 3052_나머지.java

---
+++ 2023/03/Bronze/3052_나머지.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;// = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<10;i++){
+            set.add(Integer.parseInt(br.readLine()) % 42);
+        }
+
+        System.out.println(set.size());
+    }
+}

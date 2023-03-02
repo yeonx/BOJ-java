6e07243b8576daebc227f4ed2b9d6f5d44643af4
yeonx <71878202+yeonx@users.noreply.github.com>
2023-03-02 10:21:25 +0900

    Create 13549_숨바꼭질3.java

---
+++ 2023/03/Gold/13549_숨바꼭질3.java
@@ -0,0 +1,64 @@
+package day0302;
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_13549_숨바꼭질3_서울_20반_이연희 {
+	
+	static int N,K;
+	static int answer=Integer.MAX_VALUE;
+	static int num=0;
+	static boolean visited[] = new boolean[100001];
+	
+	static void bfs() {
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		
+		q.offer(new int[] {N,0});
+		visited[N] = true;
+
+		while(!q.isEmpty()) {
+
+			int[] point = q.poll();
+			int count = point[1];
+			
+			
+			if(point[0] == K) {
+				if(answer > count)
+					answer = count;
+//				return;
+			}
+			
+			int[] a = new int[3];
+			
+			a[0] = point[0]-1;
+			a[1] = point[0]+1;
+			a[2] = 2*point[0];
+			
+			
+			if(0<=a[2] && a[2]<=100000 && !visited[a[2]]) {
+				q.offer(new int[] {a[2],count});
+				visited[a[2]] = true;
+			}
+			for(int i=0;i<2;i++) {
+				if(0<=a[i] && a[i]<=100000 && !visited[a[i]]) {
+					q.offer(new int[] {a[i],count+1});
+					visited[a[i]] = true;
+				}
+			}
+
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+	
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        
+        bfs();
+        
+        System.out.println(answer);
+	}
+
+}

f4b1f12e64353b166694704c5e80229645e78c24
yeonx <71878202+yeonx@users.noreply.github.com>
2023-03-04 17:35:45 +0900

    Create 10866_덱.java

---
+++ 2023/03/Silver/10866_덱.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_1 {
+
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        N = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<N;i++){
+            int number=-1;
+            st = new StringTokenizer(br.readLine());
+
+            String command = st.nextToken();
+            if(st.hasMoreTokens())
+                number = Integer.parseInt(st.nextToken());
+
+           if(command.equals("push_back")) q.offerLast(number);
+           else if(command.equals("push_front")) q.offerFirst(number);
+           else if(command.equals("front")) sb.append((q.isEmpty() ? "-1" : q.peekFirst()) + "\n");
+           else if(command.equals("back")) sb.append((q.isEmpty() ? "-1" : q.peekLast())+ "\n");
+           else if(command.equals("empty")) sb.append((q.isEmpty() ? "1":"0") + "\n");
+           else if(command.equals("pop_back")) sb.append((q.isEmpty() ? "-1" :q.pollLast()) + "\n");
+           else if(command.equals("pop_front")) sb.append((q.isEmpty() ? "-1" :q.pollFirst()) + "\n");
+           else if(command.equals("size")) sb.append(q.size() + "\n");
+        }
+
+        System.out.print(sb);
+    }
+}
